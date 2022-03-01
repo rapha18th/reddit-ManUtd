984f3060c4c1098e75c65c3cd5506cc5cdc89bc2
rairom <rairom@sonity.net>
2022-03-01 21:51:51 +0200

    train.py

---
+++ train.py
@@ -0,0 +1,230 @@
+# Importing the libraries needed
+import pandas as pd
+import torch
+import transformers
+from torch.utils.data import Dataset, DataLoader
+from transformers import DistilBertModel, DistilBertTokenizer
+
+import numpy as np
+from tqdm import tqdm
+
+from habana_frameworks.torch.utils.library_loader import load_habana_module
+load_habana_module()
+
+print(1+1)
+
+master_df = pd.read_csv("reddit.csv")
+print("sample: ", master_df.sample())
+
+df = pd.read_csv("reddit_subset.csv")
+
+r_ls = df['result'].tolist()
+
+
+df['result'].replace({0:"loss", 1:"draw", 3:"win"}, inplace=True)
+
+df['result_points'] = r_ls
+
+print("head: ", df.head())
+
+#Use hpu device for training.
+device = "hpu"
+print("Using {} device".format(device))
+
+df = df[['top_comment','result_points']]
+
+# # Converting the codes to appropriate categories using a dictionary
+my_dict = {
+    0:'loss',
+    1:'draw',
+    3:'win'
+}
+
+def update_cat(x):
+    return my_dict[x]
+
+df['result_points'] = df['result_points'].apply(lambda x: update_cat(x))
+
+encode_dict = {}
+
+def encode_cat(x):
+    if x not in encode_dict.keys():
+        encode_dict[x]=len(encode_dict)
+    return encode_dict[x]
+
+df['ENCODE_CAT'] = df['result_points'].apply(lambda x: encode_cat(x))
+
+# Define parameters
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 4
+VALID_BATCH_SIZE = 2
+EPOCHS = 1
+LEARNING_RATE = 1e-06
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
+
+class Triage(Dataset):
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.len = len(dataframe)
+        self.data = dataframe
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __getitem__(self, index):
+        comment = str(self.data.top_comment[index])
+        comment = " ".join(comment.split())
+        inputs = self.tokenizer.encode_plus(
+            comment,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            return_token_type_ids=True,
+            truncation=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'targets': torch.tensor(self.data.ENCODE_CAT[index], dtype=torch.long)
+        }
+
+    def __len__(self):
+        return self.len
+
+# Creating the dataset and dataloader for the neural network
+
+train_size = 0.8
+train_dataset=df.sample(frac=train_size,random_state=200)
+test_dataset=df.drop(train_dataset.index).reset_index(drop=True)
+train_dataset = train_dataset.reset_index(drop=True)
+
+
+print("Dataset: {}".format(df.shape))
+print("TRAIN : {}".format(train_dataset.shape))
+print("TEST : {}".format(test_dataset.shape))
+
+training_set = Triage(train_dataset, tokenizer, MAX_LEN)
+testing_set = Triage(test_dataset, tokenizer, MAX_LEN)
+
+train_params = {'batch_size': TRAIN_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+test_params = {'batch_size': VALID_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+training_loader = DataLoader(training_set, **train_params)
+testing_loader = DataLoader(testing_set, **test_params)
+
+class DistillBERTClass(torch.nn.Module):
+    def __init__(self):
+        super(DistillBERTClass, self).__init__()
+        self.l1 = DistilBertModel.from_pretrained("distilbert-base-uncased")
+        self.pre_classifier = torch.nn.Linear(768, 768)
+        self.dropout = torch.nn.Dropout(0.3)
+        self.classifier = torch.nn.Linear(768, 4)
+
+    def forward(self, input_ids, attention_mask):
+        output_1 = self.l1(input_ids=input_ids, attention_mask=attention_mask)
+        hidden_state = output_1[0]
+        pooler = hidden_state[:, 0]
+        pooler = self.pre_classifier(pooler)
+        pooler = torch.nn.ReLU()(pooler)
+        pooler = self.dropout(pooler)
+        output = self.classifier(pooler)
+        return output
+
+model = DistillBERTClass()
+model.to(device)
+
+# Creating the loss function and optimizer
+loss_function = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params =  model.parameters(), lr=LEARNING_RATE)
+# Function to calcuate the accuracy of the model
+
+def calcuate_accu(big_idx, targets):
+    n_correct = (big_idx==targets).sum().item()
+    return n_correct
+
+# Defining the training function on the 80% of the dataset for tuning the distilbert model
+def train(epoch):
+    tr_loss = 0
+    n_correct = 0
+    nb_tr_steps = 0
+    nb_tr_examples = 0
+    model.train()
+    for _,data in enumerate(training_loader, 0):
+        ids = data['ids'].to(device, dtype = torch.long)
+        mask = data['mask'].to(device, dtype = torch.long)
+        targets = data['targets'].to(device, dtype = torch.long)
+
+        outputs = model(ids, mask)
+        loss = loss_function(outputs, targets)
+        tr_loss += loss.item()
+        big_val, big_idx = torch.max(outputs.data, dim=1)
+        n_correct += calcuate_accu(big_idx, targets)
+
+        nb_tr_steps += 1
+        nb_tr_examples+=targets.size(0)
+
+        if _%100==0:
+            loss_step = tr_loss/nb_tr_steps
+            accu_step = (n_correct*100)/nb_tr_examples
+            print(f"Training Loss per 100 steps: {loss_step}")
+            print(f"Training Accuracy per 100 steps: {accu_step}")
+
+        optimizer.zero_grad()
+        loss.backward()
+
+        optimizer.step()
+
+    print(f'The Total Accuracy for Epoch {epoch}: {(n_correct*100)/nb_tr_examples}')
+    epoch_loss = tr_loss/nb_tr_steps
+    epoch_accu = (n_correct*100)/nb_tr_examples
+    print(f"Training Loss Epoch: {epoch_loss}")
+    print(f"Training Accuracy Epoch: {epoch_accu}")
+
+    for epoch in tqdm(range(EPOCHS)):
+    train(epoch)
+
+def valid(model, testing_loader):
+    model.eval()
+    n_correct = 0; n_wrong = 0; total = 0
+    with torch.no_grad():
+        for _, data in enumerate(testing_loader, 0):
+            ids = data['ids'].to(device, dtype = torch.long)
+            mask = data['mask'].to(device, dtype = torch.long)
+            targets = data['targets'].to(device, dtype = torch.long)
+            outputs = model(ids, mask).squeeze()
+            loss = loss_function(outputs, targets)
+            tr_loss += loss.item()
+            big_val, big_idx = torch.max(outputs.data, dim=1)
+            n_correct += calcuate_accu(big_idx, targets)
+
+            nb_tr_steps += 1
+            nb_tr_examples+=targets.size(0)
+
+            if _%100==0:
+                loss_step = tr_loss/nb_tr_steps
+                accu_step = (n_correct*100)/nb_tr_examples
+                print(f"Validation Loss per 100 steps: {loss_step}")
+                print(f"Validation Accuracy per 100 steps: {accu_step}")
+
+   # epoch_loss = tr_loss/nb_tr_steps
+    epoch_accu = (n_correct*100)/nb_tr_examples
+    #print(f"Validation Loss Epoch: {epoch_loss}")
+    print(f"Validation Accuracy Epoch: {epoch_accu}")
+
+    return epoch_accu
+
+print('Accuracy on the test data Using the dataloader created for the validation dataset')
+acc = valid(model, testing_loader)
+print("Accuracy on test data = %0.2f%%" % acc)
+
+
+
